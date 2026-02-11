1cdd4acc730d14f4da03b4cc471ef42618758819
Shivangi Kumari <shivangikumari938@gmail.com>
2026-02-11 18:00:10 +0530

    Day 02 : Learn variables and datatypes in java.

--- Day02/Datatype1.java
+++ Day02/Datatype1.java
@@ -7,7 +7,7 @@ class Datatype1{
     long l=10000000000L;
     float f=3.14f;
     double d=3.14159265359;
-    char grade='A';
+    char c='A';
     boolean flag=true;
 
   //Non-primitive Datatypes
@@ -23,7 +23,7 @@ class Datatype1{
   System.out.println("float : "+f);
   System.out.println("double : "+d);
   System.out.println("char : "+c);
-  System.out.println("boolean : "+b);
+  System.out.println("boolean : "+flag);
   System.out.println("Non-primitive DataTypes : ");
   System.out.println("String : "+str);
   System.out.println("Array : ");
